0d6122d14609bb289d4998f517aa43969c60ccec
MarkZither <mark.burton@zither-it.co.uk>
2020-12-22 23:05:17 +0100

    add a sample project using appsettings and Serilog.Settings.Configuration package

---
+++ samples/Serilog.Sinks.InfluxDB.Console.AppSettings/SampleOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.InfluxDB.Console.AppSettings
+{
+    public class SampleOptions
+    {
+        public const string Sample = "Sample";
+        public int Number { get; }
+        public SampleOptions(int number)
+        {
+            Number = number;
+        }
+
+        public SampleOptions()
+        {
+            Number = 10000;
+        }
+    }
+}
